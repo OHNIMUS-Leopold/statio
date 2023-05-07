af55445144768179f1626a4184b2eaf47fc19697
OHNIMUS-Leopold <leopold.ohnimus@edu.univ-fcomte.fr>
2023-05-07 16:28:25 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      // "colors": {
+      //   "gray": "#a1a1a1",
+      // },
+
+      // "fontFamily": {
+      //   "Poppins": "Poppins, sans-serif",
+      //   "Noto Sans KR": "Noto Sans KR, sans-serif"
+      // }
+    },
+  },
+  plugins: [],
+}
+
