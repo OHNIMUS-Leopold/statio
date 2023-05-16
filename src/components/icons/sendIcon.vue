3c24e7d4fe60e5cf738e1d4ea77934968d98bdf1
OHNIMUS-Leopold <leopold.ohnimus@edu.univ-fcomte.fr>
2023-05-16 08:55:53 +0200

    import icons (partie 1)

---
+++ src/components/icons/sendIcon.vue
@@ -0,0 +1,5 @@
+<template>
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M10.1101 13.6499L13.6901 10.0599M7.40005 6.31991L15.8901 3.48991C19.7001 2.21991 21.7701 4.29991 20.5101 8.10991L17.6801 16.5999C15.7801 22.3099 12.6601 22.3099 10.7601 16.5999L9.92005 14.0799L7.40005 13.2399C1.69005 11.3399 1.69005 8.22991 7.40005 6.31991Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+</template>
