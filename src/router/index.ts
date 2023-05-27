7cd1679ae6e273029ace3060fc42fd5653e337ba
OHNIMUS-Leopold <leopold.ohnimus@edu.univ-fcomte.fr>
2023-05-27 16:33:58 +0200

    pages menu créées + router à jour

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
+import DocsView from '../views/DocsView.vue'
+import MapView from '../views/MapView.vue'
+import TranslateView from '../views/TranslateView.vue'
+import ChatPageView from '../views/ChatPageView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/',              name: 'home',           component: HomeView     },
-    { path: '/about',         name: 'about',          component: AboutView    },
+    { path: '/',              name: 'home',           component: HomeView       },
+    { path: '/docs',          name: 'docs',           component: DocsView       },
+    { path: '/map',           name: 'map',            component: MapView        },
+    { path: '/translate',     name: 'translate',      component: TranslateView  },
+    { path: '/chatpage',      name: 'chatpage',       component: ChatPageView   },
   ]
 })
 
