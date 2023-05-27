9cf4bf716f01b21bb07b245b91c442279352546f
OHNIMUS-Leopold <leopold.ohnimus@edu.univ-fcomte.fr>
2023-05-27 16:51:09 +0200

    menu terminé

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,36 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-// import HelloWorld from './components/HelloWorld.vue'
+import HomeIcon from '@/components/icons/homeIcon.vue';
+import DocsIcon from '@/components/icons/docsIcon.vue';
+import MapIcon from '@/components/icons/mapIcon.vue';
+import TranslateIconVue from '@/components/icons/translateIcon.vue';
+import MessagesIconVue from '@/components/icons/messagesIcon.vue';
 </script>
 
 <template>
-  <header>
-    <div>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
 
-  <RouterView />
-  
+    <RouterView />
+
+    <footer>
+      <div class="fixed bottom-0 w-full z-10 bg-darkblue px-7 py-6 rounded-t-3xl">
+        <div class="flex justify-between">
+          <RouterLink to="/">
+            <HomeIcon class="opacity-60" />
+          </RouterLink>
+          <RouterLink to="/docs">
+            <DocsIcon class="opacity-60" />
+          </RouterLink>
+          <RouterLink to="/map">
+            <MapIcon class="opacity-60" />
+          </RouterLink>
+          <RouterLink to="/translate">
+            <TranslateIconVue class="opacity-60" />
+          </RouterLink>
+          <RouterLink to="/chatpage">
+            <MessagesIconVue class="opacity-60" />
+          </RouterLink>
+        </div>
+      </div>
+    </footer>
+    
 </template>
-
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,6 @@
 <script setup lang="ts">
-import HomeIcon from '@/components/icons/homeIcon.vue';
-import DocsIcon from '@/components/icons/docsIcon.vue';
-import MapIcon from '@/components/icons/mapIcon.vue';
-import TranslateIconVue from '@/components/icons/translateIcon.vue';
-import MessagesIconVue from '@/components/icons/messagesIcon.vue';
 </script>
 
 <template>
-  <div>
-    <RouterView />
-    <!-- <RouterLink></RouterLink> -->
-
-    <div class="bg-darkblue px-7 py-6 rounded-t-3xl">   
-      <div class="flex justify-between">
-        <RouterLink to="">
-          <HomeIcon />
-        </RouterLink>
-        <RouterLink to="">
-          <DocsIcon />
-        </RouterLink>
-        <RouterLink to="">
-          <MapIcon />
-        </RouterLink>
-        <RouterLink to="">
-          <TranslateIconVue />
-        </RouterLink>
-        <RouterLink to="">
-          <MessagesIconVue />
-        </RouterLink>
-      </div>
-    </div>
-    
-  </div>
+  <div></div>
 </template>
