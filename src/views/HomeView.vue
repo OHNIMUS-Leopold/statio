b28bdc33cb748fd7d36603d11db5b4b32a362671
OHNIMUS-Leopold <leopold.ohnimus@edu.univ-fcomte.fr>
2023-05-27 16:25:24 +0200

    création du menu

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,35 @@
 <script setup lang="ts">
+import HomeIcon from '@/components/icons/homeIcon.vue';
+import DocsIcon from '@/components/icons/docsIcon.vue';
+import MapIcon from '@/components/icons/mapIcon.vue';
+import TranslateIconVue from '@/components/icons/translateIcon.vue';
+import MessagesIconVue from '@/components/icons/messagesIcon.vue';
 </script>
 
 <template>
   <div>
-    <h1 class="h1-mobile">This is an home page</h1>
+    <RouterView />
+    <!-- <RouterLink></RouterLink> -->
+
+    <div class="bg-darkblue px-7 py-6 rounded-t-3xl">   
+      <div class="flex justify-between">
+        <RouterLink to="">
+          <HomeIcon />
+        </RouterLink>
+        <RouterLink to="">
+          <DocsIcon />
+        </RouterLink>
+        <RouterLink to="">
+          <MapIcon />
+        </RouterLink>
+        <RouterLink to="">
+          <TranslateIconVue />
+        </RouterLink>
+        <RouterLink to="">
+          <MessagesIconVue />
+        </RouterLink>
+      </div>
+    </div>
+    
   </div>
 </template>
