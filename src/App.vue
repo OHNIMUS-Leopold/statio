4e19ba0bf3995514823176f3d86a0bb5c6ed3484
OHNIMUS-Leopold <leopold.ohnimus@edu.univ-fcomte.fr>
2023-05-27 17:20:44 +0200

    menu footer terminé avec classe sur lien actif

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView, useRoute } from 'vue-router'
 import HomeIcon from '@/components/icons/homeIcon.vue';
 import DocsIcon from '@/components/icons/docsIcon.vue';
 import MapIcon from '@/components/icons/mapIcon.vue';
 import TranslateIconVue from '@/components/icons/translateIcon.vue';
 import MessagesIconVue from '@/components/icons/messagesIcon.vue';
+
+const route = useRoute(); // Récupère le chemin de la page active
 </script>
 
 <template>
@@ -13,23 +15,33 @@ import MessagesIconVue from '@/components/icons/messagesIcon.vue';
 
     <footer>
       <div class="fixed bottom-0 w-full z-10 bg-darkblue px-7 py-6 rounded-t-3xl">
-        <div class="flex justify-between">
-          <RouterLink to="/">
-            <HomeIcon class="opacity-60" />
-          </RouterLink>
-          <RouterLink to="/docs">
-            <DocsIcon class="opacity-60" />
-          </RouterLink>
-          <RouterLink to="/map">
-            <MapIcon class="opacity-60" />
-          </RouterLink>
-          <RouterLink to="/translate">
-            <TranslateIconVue class="opacity-60" />
-          </RouterLink>
-          <RouterLink to="/chatpage">
-            <MessagesIconVue class="opacity-60" />
-          </RouterLink>
-        </div>
+        <nav class="flex justify-between">
+          <li class="list-none">
+            <RouterLink to="/" :class="route.path !== '/' ? 'opacity-60' : 'opacity-100'">
+              <HomeIcon />
+            </RouterLink>
+          </li>
+          <li class="list-none">
+            <RouterLink to="/docs" :class="route.path !== '/docs' ? 'opacity-60' : 'opacity-100'">
+              <DocsIcon />
+            </RouterLink>
+          </li>
+          <li class="list-none">
+            <RouterLink to="/map" :class="route.path !== '/map' ? 'opacity-60' : 'opacity-100'">
+              <MapIcon />
+            </RouterLink>
+          </li>
+          <li class="list-none">
+            <RouterLink to="/translate" :class="route.path !== '/translate' ? 'opacity-60' : 'opacity-100'">
+              <TranslateIconVue />
+            </RouterLink>
+          </li>
+          <li class="list-none">
+            <RouterLink to="/chatpage" :class="route.path !== '/chatpage' ? 'opacity-60' : 'opacity-100'">
+              <MessagesIconVue />
+            </RouterLink>
+          </li>
+        </nav>
       </div>
     </footer>
     
