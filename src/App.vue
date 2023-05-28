5bf47b5ad8e30d094863d2f61c0be36b925af78d
OHNIMUS-Leopold <leopold.ohnimus@edu.univ-fcomte.fr>
2023-05-28 12:53:36 +0200

    header fini

--- src/App.vue
+++ src/App.vue
@@ -3,46 +3,57 @@ import { RouterLink, RouterView, useRoute } from 'vue-router'
 import HomeIcon from '@/components/icons/homeIcon.vue';
 import DocsIcon from '@/components/icons/docsIcon.vue';
 import MapIcon from '@/components/icons/mapIcon.vue';
-import TranslateIconVue from '@/components/icons/translateIcon.vue';
-import MessagesIconVue from '@/components/icons/messagesIcon.vue';
+import TranslateIcon from '@/components/icons/translateIcon.vue';
+import MessagesIcon from '@/components/icons/messagesIcon.vue';
+import userFoto from '@/components/userFoto.vue';
 
 const route = useRoute(); // Récupère le chemin de la page active
 </script>
 
 <template>
 
-    <RouterView />
+  <header>
+    <div class="flex px-5 pt-12 justify-between items-center">
+      <p class="logo">
+        Statio
+      </p>
+      <userFoto imgPath="./src/assets/img/template/user.png" />
+    </div>
+  </header>
+  
+  <RouterView />
 
-    <footer>
-      <div class="fixed bottom-0 w-full z-10 bg-darkblue px-7 py-6 rounded-t-3xl">
-        <nav class="flex justify-between">
-          <li class="list-none">
-            <RouterLink to="/" :class="route.path !== '/' ? 'opacity-60' : 'opacity-100'">
-              <HomeIcon />
-            </RouterLink>
-          </li>
-          <li class="list-none">
-            <RouterLink to="/docs" :class="route.path !== '/docs' ? 'opacity-60' : 'opacity-100'">
-              <DocsIcon />
-            </RouterLink>
-          </li>
-          <li class="list-none">
-            <RouterLink to="/map" :class="route.path !== '/map' ? 'opacity-60' : 'opacity-100'">
-              <MapIcon />
-            </RouterLink>
-          </li>
-          <li class="list-none">
-            <RouterLink to="/translate" :class="route.path !== '/translate' ? 'opacity-60' : 'opacity-100'">
-              <TranslateIconVue />
-            </RouterLink>
-          </li>
-          <li class="list-none">
-            <RouterLink to="/chatpage" :class="route.path !== '/chatpage' ? 'opacity-60' : 'opacity-100'">
-              <MessagesIconVue />
-            </RouterLink>
-          </li>
-        </nav>
-      </div>
-    </footer>
+  <footer>
+    <div class="fixed bottom-0 w-full z-10 bg-darkblue px-7 py-6 rounded-t-3xl">
+      <nav class="flex justify-between">
+        <li class="list-none">
+          <RouterLink to="/" :class="route.path !== '/' ? 'opacity-60' : 'opacity-100'">
+            <HomeIcon />
+          </RouterLink>
+        </li>
+        <li class="list-none">
+          <RouterLink to="/docs" :class="route.path !== '/docs' ? 'opacity-60' : 'opacity-100'">
+            <DocsIcon />
+          </RouterLink>
+        </li>
+        <li class="list-none">
+          <RouterLink to="/map" :class="route.path !== '/map' ? 'opacity-60' : 'opacity-100'">
+            <MapIcon />
+          </RouterLink>
+        </li>
+        <li class="list-none">
+          <RouterLink to="/translate" :class="route.path !== '/translate' ? 'opacity-60' : 'opacity-100'">
+            <TranslateIcon />
+          </RouterLink>
+        </li>
+        <li class="list-none">
+          <RouterLink to="/chatpage" :class="route.path !== '/chatpage' ? 'opacity-60' : 'opacity-100'">
+            <MessagesIcon />
+          </RouterLink>
+        </li>
+      </nav>
+    </div>
+  </footer>
     
 </template>
+
---
+++ src/components/userFoto.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps<{
+  imgPath: string
+}>()
+</script>
+
+<template>
+    <div class="w-10 h-10 relative">
+        <img class="absolute left-[-1px] top-[-1px]" :src="imgPath" alt="photo de profil" />
+        <svg
+            width="12"
+            height="12"
+            viewBox="0 0 12 12"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            class="absolute left-[30.5px] top-[-1.5px]"
+            preserveAspectRatio="xMidYMid meet"
+            >
+            <circle cx="6" cy="6" r="5" fill="#FF8383" stroke="#F9F9FB" stroke-width="2"></circle>
+        </svg>
+    </div>
+</template>
