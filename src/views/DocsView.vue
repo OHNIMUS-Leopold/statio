9e8ecd5ce71283eba55f19ff194b12b1b9a4ff58
OHNIMUS-Leopold <leopold.ohnimus@edu.univ-fcomte.fr>
2023-05-28 17:15:28 +0200

    arrondi du résultat

--- src/views/DocsView.vue
+++ src/views/DocsView.vue
@@ -6,17 +6,18 @@ import HeaderComp from '@/components/header.vue';
     
     <HeaderComp />
     
-    <div class="bg-blanc py-4 mx-5 px-4 shadow-drop2">
+    <div class="bg-blanc mt-7 py-4 mx-5 px-4 shadow-drop2 rounded-xl">
         <h1 class="text-center h4-mobile">
             Convertisseur de devise
         </h1>
+        <hr class="border-lightgray mt-2 mb-4">
         <form
-            class="mx-auto w-full max-w-sm bg-white shadow rounded-md p-5 space-y-3 text-sm"
+            class=""
             @submit.prevent="submit"
         >
-            <div class="flex items-center justify-between space-x-5">
-                <label for="base_currency_input">Base currency:</label>
-                <select v-model="params.base_currency" name="currency_choice" id="currency_choice">
+            <div class="">
+                <label for="base_currency_input" class="hidden">Base currency:</label>
+                <select v-model="params.base_currency" class="text-darkgray font-poppins font-semibold text-base py-2 px-3 outline rounded-xl" name="currency_choice" id="currency_choice">
                     <option value="CAD">Canada</option>
                     <option value="GBP">England</option>
                     <option value="EUR">France</option>
@@ -25,24 +26,45 @@ import HeaderComp from '@/components/header.vue';
                     <option value="USD">United States</option>
                 </select>
             </div>
-            <div class="flex items-center justify-between space-x-5">
-                <label for="currencies">Target currencies:</label>
-                <input
-                    v-model="params.currencies"
-                    type="text"
-                    class="border-slate-300 border rounded-md py-2 px-4 text-sm"
-                />
+            <div class="flex mt-4 ml-3 items-center">
+                <div class="">
+                    <label for="multiplier" class="hidden">Multiplicateur :</label>
+                    <input v-model="multiplier" type="number" class="w-48 text-blue font-poppins font-normal text-5xl" />
+                </div>
+                <div>
+                    <p class="text">{{ params.base_currency }}</p>
+                </div>
             </div>
-            <div class="flex items-center justify-between space-x-5">
-                <label for="multiplier">Multiplicateur :</label>
-                <input v-model="multiplier" type="number" class="border border-gray-300 rounded-md py-2 px-4 text-sm" />
+            <div class="text-center">
+                <button
+                    type="submit"
+                    class=""
+                >Convertir
+                </button>
             </div>
-            <button
-                type="submit"
-                class="bg-slate-800 text-white rounded-md py-2 px-4 mx-auto relative block"
-            >Get Latest Rates</button>
-        </form>        
+            <div class="">
+                <label for="currencies" class="hidden">Target currencies:</label>
+                <select v-model="params.currencies" class="text-darkgray font-poppins font-semibold text-base py-2 px-3 outline rounded-xl" name="currencies_choice" id="currencies_choice">
+                    <option value="CAD">Canada</option>
+                    <option value="GBP">England</option>
+                    <option value="EUR">France</option>
+                    <option value="JPY">Japan</option>
+                    <option value="KRW">South Korea</option>
+                    <option value="USD">United States</option>
+                </select>
+            </div>
             <div
+                v-for="result of results"
+                :key="result.code"
+                class="flex mt-4 ml-3 items-center"
+            >
+            <p class="w-48 text-blue font-poppins font-normal text-5xl">{{ multiplyResult(result.value) }}</p>
+            <p class="text">{{ result.code }}</p>
+                
+            </div>
+        </form>
+                
+            <!-- <div
                 v-for="result of results"
                 :key="result.code"
                 class="flex justify-between"
@@ -50,7 +72,7 @@ import HeaderComp from '@/components/header.vue';
                 <strong>{{ result.code }}</strong>
                 <span>{{ result.value }}</span>
                 <span >{{ multiplyResult(result.value) }}</span>
-            </div>
+            </div> -->
         
 
         
@@ -69,7 +91,7 @@ export default {
         return {
             params: {
                 base_currency: 'USD',
-                currencies: 'EUR,CAD'
+                currencies: 'CAD'
             },
             results: null,
             currencyApi: new CurrencyAPI('45ORre8htUcRSe3R7G770MVE6R0JmUHbq3id6j4U'),
@@ -88,7 +110,7 @@ export default {
         multiplyResult(value) {
         const multiplierValue = parseFloat(this.multiplier);
         if (!isNaN(multiplierValue)) {
-            return value * multiplierValue;
+            return Math.round((value * multiplierValue) * 100) / 100;
         } else {
             return value;
         }
