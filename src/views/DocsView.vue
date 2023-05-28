73e6fbce0edf1339f69d895b19e60f5a650109e5
OHNIMUS-Leopold <leopold.ohnimus@edu.univ-fcomte.fr>
2023-05-28 18:07:49 +0200

    convertisseur terminé

--- src/views/DocsView.vue
+++ src/views/DocsView.vue
@@ -17,7 +17,7 @@ import HeaderComp from '@/components/header.vue';
         >
             <div class="">
                 <label for="base_currency_input" class="hidden">Base currency:</label>
-                <select v-model="params.base_currency" class="text-darkgray font-poppins font-semibold text-base py-2 px-3 outline rounded-xl" name="currency_choice" id="currency_choice">
+                <select v-model="params.base_currency" class="text-darkgray focus:outline-darkblue focus:text-darkblue font-poppins font-semibold text-base py-2 px-3 outline outline-2 rounded-xl" name="currency_choice" id="currency_choice">
                     <option value="CAD">Canada</option>
                     <option value="GBP">England</option>
                     <option value="EUR">France</option>
@@ -26,7 +26,7 @@ import HeaderComp from '@/components/header.vue';
                     <option value="USD">United States</option>
                 </select>
             </div>
-            <div class="flex mt-4 ml-3 items-center">
+            <div class="flex mt-4 ml-3 mr-7 items-center justify-between">
                 <div class="">
                     <label for="multiplier" class="hidden">Multiplicateur :</label>
                     <input v-model="multiplier" type="number" class="w-48 text-blue font-poppins font-normal text-5xl" />
@@ -38,13 +38,13 @@ import HeaderComp from '@/components/header.vue';
             <div class="text-center">
                 <button
                     type="submit"
-                    class=""
+                    class="shadow-drop-shadow degrade font-poppins py-2 px-6 mx-auto mt-3 mb-6 rounded-3xl text-base font-medium text-center text-blanc"
                 >Convertir
                 </button>
             </div>
             <div class="">
                 <label for="currencies" class="hidden">Target currencies:</label>
-                <select v-model="params.currencies" class="text-darkgray font-poppins font-semibold text-base py-2 px-3 outline rounded-xl" name="currencies_choice" id="currencies_choice">
+                <select v-model="params.currencies" class="text-darkgray focus:outline-darkblue focus:text-darkblue font-poppins font-semibold text-base py-2 px-3 outline outline-2 rounded-xl" name="currencies_choice" id="currencies_choice">
                     <option value="CAD">Canada</option>
                     <option value="GBP">England</option>
                     <option value="EUR">France</option>
@@ -56,10 +56,10 @@ import HeaderComp from '@/components/header.vue';
             <div
                 v-for="result of results"
                 :key="result.code"
-                class="flex mt-4 ml-3 items-center"
+                class="flex mt-4 ml-3 mr-7 items-center justify-between"
             >
-            <p class="w-48 text-blue font-poppins font-normal text-5xl">{{ multiplyResult(result.value) }}</p>
-            <p class="text">{{ result.code }}</p>
+            <p class="pt-2 w-48 text-blue font-poppins font-normal text-5xl">{{ multiplyResult(result.value) }}</p>
+            <p class="pt-2 text">{{ result.code }}</p>
                 
             </div>
         </form>
@@ -90,11 +90,11 @@ export default {
     data () {
         return {
             params: {
-                base_currency: 'USD',
-                currencies: 'CAD'
+                base_currency: 'EUR',
+                currencies: 'USD'
             },
             results: null,
-            currencyApi: new CurrencyAPI('45ORre8htUcRSe3R7G770MVE6R0JmUHbq3id6j4U'),
+            currencyApi: new CurrencyAPI('API-KEY'),
             multiplier: 1,
         }
     },
