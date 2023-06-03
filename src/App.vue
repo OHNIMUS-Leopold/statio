9ff19ad69ed03f0e4cf9b8f79aaae3275f02cc24
OHNIMUS-Leopold <leopold.ohnimus@edu.univ-fcomte.fr>
2023-06-03 18:47:59 +0200

    footer en composant

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,14 @@
 <script setup lang="ts">
 import { RouterLink, RouterView, useRoute } from 'vue-router'
-import HomeIcon from '@/components/icons/homeIcon.vue';
-import DocsIcon from '@/components/icons/docsIcon.vue';
-import MapIcon from '@/components/icons/mapIcon.vue';
-import TranslateIcon from '@/components/icons/translateIcon.vue';
-import MessagesIcon from '@/components/icons/messagesIcon.vue';
+import footerComp from './components/footer.vue';
 
-const route = useRoute(); // Récupère le chemin de la page active
 </script>
 
 <template>
 
   <RouterView />
 
-  <footer>
-    <div class="fixed bottom-0 w-full z-10 bg-darkblue px-7 py-6 rounded-t-3xl">
-      <nav class="flex justify-between">
-        <li class="list-none">
-          <RouterLink to="/" :class="route.path !== '/' ? 'opacity-60' : 'opacity-100'">
-            <HomeIcon />
-          </RouterLink>
-        </li>
-        <li class="list-none">
-          <RouterLink to="/docs" :class="route.path !== '/docs' ? 'opacity-60' : 'opacity-100'">
-            <DocsIcon />
-          </RouterLink>
-        </li>
-        <li class="list-none">
-          <RouterLink to="/map" :class="route.path !== '/map' ? 'opacity-60' : 'opacity-100'">
-            <MapIcon />
-          </RouterLink>
-        </li>
-        <li class="list-none">
-          <RouterLink to="/translate" :class="route.path !== '/translate' ? 'opacity-60' : 'opacity-100'">
-            <TranslateIcon />
-          </RouterLink>
-        </li>
-        <li class="list-none">
-          <RouterLink to="/chatpage" :class="route.path !== '/chatpage' ? 'opacity-60' : 'opacity-100'">
-            <MessagesIcon />
-          </RouterLink>
-        </li>
-      </nav>
-    </div>
-  </footer>
+  <footerComp />
     
 </template>
 
---
+++ src/components/footer.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { RouterLink, RouterView, useRoute } from 'vue-router'
+import homeIcon from '@/components/icons/homeIcon.vue';
+import docsIcon from '@/components/icons/docsIcon.vue';
+import mapIcon from '@/components/icons/mapIcon.vue';
+import translateIcon from '@/components/icons/translateIcon.vue';
+import messagesIcon from '@/components/icons/messagesIcon.vue';
+
+const route = useRoute(); // Récupère le chemin de la page active
+</script>
+
+<template>
+    <footer>
+    <div class="fixed bottom-0 w-full z-10 bg-darkblue px-7 py-6 rounded-t-3xl">
+      <nav class="flex justify-between">
+        <li class="list-none">
+          <RouterLink to="/" :class="route.path !== '/' ? 'opacity-60' : 'opacity-100'">
+            <homeIcon />
+          </RouterLink>
+        </li>
+        <li class="list-none">
+          <RouterLink to="/docs" :class="route.path !== '/docs' ? 'opacity-60' : 'opacity-100'">
+            <docsIcon />
+          </RouterLink>
+        </li>
+        <li class="list-none">
+          <RouterLink to="/map" :class="route.path !== '/map' ? 'opacity-60' : 'opacity-100'">
+            <mapIcon />
+          </RouterLink>
+        </li>
+        <li class="list-none">
+          <RouterLink to="/translate" :class="route.path !== '/translate' ? 'opacity-60' : 'opacity-100'">
+            <translateIcon />
+          </RouterLink>
+        </li>
+        <li class="list-none">
+          <RouterLink to="/chatpage" :class="route.path !== '/chatpage' ? 'opacity-60' : 'opacity-100'">
+            <messagesIcon />
+          </RouterLink>
+        </li>
+      </nav>
+    </div>
+  </footer>
+</template>
