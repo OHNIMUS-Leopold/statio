0ea431db302321d02be74493615be86601bb969f
OHNIMUS-Leopold <leopold.ohnimus@edu.univ-fcomte.fr>
2023-06-05 15:22:53 +0200

    nouvelles routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,11 +4,15 @@ import DocsView from '../views/DocsView.vue'
 import MapView from '../views/MapView.vue'
 import TranslateView from '../views/TranslateView.vue'
 import ChatPageView from '../views/ChatPageView.vue'
+import LandingView from '../views/LandingView.vue'
+import ConnexionView from '../views/ConnexionView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/',              name: 'home',           component: HomeView       },
+    { path: '/',              name: 'landing',        component: LandingView    },
+    { path: '/connexion',     name: 'connexion',      component: ConnexionView  },
+    { path: '/home',          name: 'home',           component: HomeView       },
     { path: '/docs',          name: 'docs',           component: DocsView       },
     { path: '/map',           name: 'map',            component: MapView        },
     { path: '/translate',     name: 'translate',      component: TranslateView  },
