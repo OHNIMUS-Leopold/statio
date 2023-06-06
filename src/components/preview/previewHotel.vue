a6ea02c36acae5e683bfeb9676d74a2069c3df2f
OHNIMUS-Leopold <leopold.ohnimus@edu.univ-fcomte.fr>
2023-06-06 21:34:39 +0200

    liste hotels home

---
+++ src/components/preview/previewHotel.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { pb } from '@/backend'
+import locationIcon from '../icons/locationIcon.vue';
+import starIcon from '../icons/starIcon.vue';
+import rightIcon from '../icons/rightIcon.vue';
+import type { HotelResponse } from '@/pocketbase-types'
+
+
+const props = defineProps<HotelResponse>()
+
+const img0 = props.preview
+const urlImg0 = img0 ? pb.getFileUrl(props, img0, { thumb: '100x250' }) : '/image-not-found.png'
+
+
+
+
+</script>
+
+
+<template>
+    <div class="col-span-1 bg-blanc rounded-2xl shadow-drop2 pb-[10px]">
+        <div class="p-[10px]">
+            <img class="rounded-lg" :src="urlImg0" alt="Photo de {{ nom }}">
+        </div>
+        <h3 class="mx-[10px] mb-[2px] font-poppins font-semibold text-xs text-black">{{ nom }}</h3>
+        <div class="grid grid-cols-5 mx-[10px] items-center">
+            <div class="col-span-3">
+                <div class="grid grid-cols-3">
+                    <div class="col-span-1">
+                        <locationIcon class="h-3 w-3" />
+                    </div>
+                    <div class="col-span-2 -ml-3">
+                        <p class="font-poppins font-normal text-[10px] text-black">{{ localisation.substring(0, 8) }}...</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-span-1 flex items-center">
+                <starIcon class="h-[10px] w-[10px]" />
+                <p class="font-poppins font-normal text-[10px] text-black">{{ note }}</p>
+            </div>
+            <div class="col-span-1">
+                <RouterLink :to="`/hotelById/${ id }`">
+                    <rightIcon class="w-5 h-5 bg-blue rounded-full p-[5px] ml-2" />
+                </RouterLink>
+            </div>
+        </div>
+    </div>
+</template>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -5,11 +5,12 @@ import FooterComp from '@/components/footer.vue';
 import previewVille from '@/components/preview/previewVille.vue';
 import previewActivite from '@/components/preview/previewActivite.vue';
 import previewTransport from '@/components/preview/previewTransport.vue';
+import previewHotel from '@/components/preview/previewHotel.vue';
 import tickIcon from '@/components/icons/tickIcon.vue';
 import type { VilleResponse, VilleRecord, BaseSystemFields } from '@/pocketbase-types';
-import type { ActiviteResponse, TransportResponse } from '@/pocketbase-types';
+import type { ActiviteResponse, TransportResponse, HotelResponse } from '@/pocketbase-types';
 import { allVilles } from '@/backend';
-import { allActivitesByVilleId, allTransportsByVilleId } from '@/backend';
+import { allActivitesByVilleId, allTransportsByVilleId, allHotelsByVilleId } from '@/backend';
 
 const ListeVilles = await allVilles();
 
@@ -25,12 +26,15 @@ const LesTransportsDeParis:TransportResponse[] =  TransportsParis.expand['transp
 const TransportsLondres = await allTransportsByVilleId('bb1fa1srm8wu67w')
 const LesTransportsDeLondres:TransportResponse[] =  TransportsLondres.expand['transport']
 
+const HotelsParis = await allHotelsByVilleId('4z5kd1ocz8z0kc1')
+const LesHotelsDeParis:HotelResponse[] =  HotelsParis.expand['hotel']
+
 </script>
 
 <template>
     <HeaderComp />
     
-    <main class="mb-80">
+    <main class="mb-28">
         <div class="mt-7">
             <h1 class="vingt-med text-black ml-7 mb-5">Destinations populaires</h1>
             <div class="flex overflow-x-scroll scroll-smooth snap-mandatory snap-x overflow-hidden mx-3">
@@ -58,6 +62,10 @@ const LesTransportsDeLondres:TransportResponse[] =  TransportsLondres.expand['tr
                 <h2 class="seize-norm text-center text-black mb-5 mt-9">Les transports sur Paris</h2>
                 <div class="flex flex-col gap-[10px]">
                     <previewTransport v-for="transport in LesTransportsDeParis" :key="transport.id" v-bind="{ ...transport}" />
+                </div>
+                <h2 class="seize-norm text-center text-black mb-5 mt-9">Les hôtels sur Paris</h2>
+                <div class="grid grid-cols-2 gap-[10px]">
+                    <previewHotel v-for="hotel in LesHotelsDeParis" :key="hotel.id" v-bind="{ ...hotel}" />
                 </div>
             </div>
 
