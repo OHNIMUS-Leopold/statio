b0b017041513498d750349992415840d8a257b49
OHNIMUS-Leopold <leopold.ohnimus@edu.univ-fcomte.fr>
2023-06-06 21:42:44 +0200

    idem hotels londres

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -29,6 +29,8 @@ const LesTransportsDeLondres:TransportResponse[] =  TransportsLondres.expand['tr
 const HotelsParis = await allHotelsByVilleId('4z5kd1ocz8z0kc1')
 const LesHotelsDeParis:HotelResponse[] =  HotelsParis.expand['hotel']
 
+const HotelsLondres = await allHotelsByVilleId('bb1fa1srm8wu67w')
+const LesHotelsDeLondres:HotelResponse[] =  HotelsLondres.expand['hotel']
 </script>
 
 <template>
@@ -78,6 +80,10 @@ const LesHotelsDeParis:HotelResponse[] =  HotelsParis.expand['hotel']
                 <div class="flex flex-col gap-[10px]">
                     <previewTransport v-for="transport in LesTransportsDeLondres" :key="transport.id" v-bind="{ ...transport}" />
                 </div>
+                <h2 class="seize-norm text-center text-black mb-5 mt-9">Les hôtels sur Londres</h2>
+                <div class="grid grid-cols-2 gap-[10px]">
+                    <previewHotel v-for="hotel in LesHotelsDeLondres" :key="hotel.id" v-bind="{ ...hotel}" />
+                </div>
             </div>
         </div>
     </main>
@@ -100,167 +106,3 @@ export default {
 }
 
 </script>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!-- <template>
-    <HeaderComp />
-
-
-    <div >
-       <h1 >
-         connectez-vous
-       </h1>
-       <form @submit.prevent="onCnx()" >
-         <input style="filter:drop-shadow(0px 10px 10px gray)"  type="email" id="email" name="email" placeholder="Adresse e-mail" v-model="user.email" required>
-   
-         <input style="filter:drop-shadow(0px 10px 10px gray)"  id="password" type="password" name="password" placeholder="Mot de passe" v-model="user.password" required>
-   
-         <input type="submit" value="SE CONNECTER">
-       
-       <div class="flex justify-center">
-         <p class="text-white font-Poppins font-normal text-[15px] leading-[26px] tracking-normal">Pas de compte ? Commandez votre billet et inscrivez-vous</p>
-         <RouterLink to="/inscription"><p class="text-deep-blue font-Poppins font-normal text-[15px] leading-[26px] tracking-normal"> ici !</p></RouterLink>
-       </div>
-       <div class="flex justify-center mb-[22px]">
-         <button @click="onDcnx()" class="text-deep-blue font-Poppins font-normal text-[15px] leading-[26px] tracking-normal">Se déconnecter</button>
-       </div>
-       <div class="alert alert-warning flex justify-center text-black font-Poppins font-normal text-[15px] leading-[26px] tracking-normal mb-[22px]" v-if="message!=null" >
-         {{ message }}
-       </div>
-       <div v-if="valeur!=null" class="flex justify-center pb-12">
-         <RouterLink to="/profil"><button class="text-black">Accéder à ma page de profil</button></RouterLink>
-       </div>
-       </form>
-     </div>
-
-     <FooterComp />
-
-   </template> -->
-   
-   <!-- <script lang="ts">
-   
-   
-   
-   import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js'
-   
-   import {
-       getFirestore,
-       collection,
-       doc,
-       getDocs,
-       addDoc,
-       updateDoc,
-       deleteDoc,
-       onSnapshot } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js'
-   
-       export default {
-           data(){
-               return{
-                   user:{
-                       email:null,
-                       password:null
-                   },
-                   message:null,
-                   valeur:null,
-               }
-           },
-   
-           mounted(){
-               let user = getAuth().currentUser;
-               if (user){
-                   this.user = user;
-                   this.message = "Utilisateur déjà connecté";
-                   this.valeur = 1;
-               }else{
-                   this.message = "Utilisateur non connecté";
-                   this.valeur = null;
-               }
-           },
-   
-           methods:{
-               onCnx(){
-                   signInWithEmailAndPassword(getAuth(), this.user.email, this.user.password)
-                   .then((response)=>{
-                       console.log('Utilisateur connecté', response.user);
-                       this.user = response.user;
-                       this.message = "Utilisateur connecté";
-                       this.valeur = 1;
-                       this.$router.replace('profil');
-                   })
-                   .catch((error)=>{
-                       console.log("Erreur connexion", error);
-                       this.message = "Erreur d'authentification";
-                       this.valeur = null;
-                      
-                   })
-               },
-               onDcnx(){
-                   signOut(getAuth())
-                   .then((response) =>{
-                       this.user = getAuth().currentUser;
-                       this.user = {
-                           email:null,
-                           password:null
-                       };
-                       console.log("Utilisateur deconnecté ", this.user);
-                       this.message = "Utilisateur non connecté";
-                       this.valeur = null;
-                       this.$router.replace('connexion');
-                   })
-                   .catch((error)=>{
-                       console.log("Erreur deconnexion", error);
-                   })
-               },
-           
-           },
-   
-     
-   
-       }
-   
-   
-   </script> -->
